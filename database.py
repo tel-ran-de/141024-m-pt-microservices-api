deccd76f0d425921ea5ef88778d9467280ff97fe
Maks Iamchuk <maks.iamchuk@outlook.com>
2025-03-13 21:28:17 +0500

    Урок 13: настройка пула соединений для базы данных

--- database.py
+++ database.py
@@ -1,13 +1,23 @@
-from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from typing import List
 from config import settings
 
 
-engine = create_async_engine(settings.async_database_url)
-AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+# создание движка с пулом подключений
+engine = create_async_engine(
+    settings.async_database_url,
+    pool_size=5,          # Сколько соединений держать всегда открытыми
+    max_overflow=10,      # Сколько доп. соединений открыть при пиковой нагрузке
+    pool_timeout=30,      # Как долго ждать освобождения коннекта (сек)
+    pool_recycle=1800,    # Время жизни одного соединения (секунды)
+    echo=False,            # (опционально) логирует SQL-запросы
+)
 
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
 
 async def get_db():
     async with AsyncSessionLocal() as db:
