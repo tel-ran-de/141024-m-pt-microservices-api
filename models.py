0390857d967567ac04c22bc5b0dfe563f2da720a
Maks Iamchuk <maks.iamchuk@outlook.com>
2025-03-14 14:12:28 +0500

    Урок 14: добавление модели пользователя

--- models.py
+++ models.py
@@ -83,3 +83,11 @@ class FoundItem(Base):
         secondary=founditem_tag,   # та же таблица founditem_tag
         back_populates="found_items"
     )
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String, unique=True, index=True)
+    hashed_password: Mapped[str] = mapped_column(String)
