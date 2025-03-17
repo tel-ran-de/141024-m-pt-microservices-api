c37e0e180060beaeef7482523239f37029934df4
Maks Iamchuk <maks.iamchuk@outlook.com>
2025-03-17 15:34:36 +0500

    Урок 15: настройка Nginx и обновление конфигурации FastAPI

--- auth_service/main.py
+++ auth_service/main.py
@@ -1,11 +1,21 @@
-from fastapi import FastAPI, Depends, HTTPException
-from routers.users import router as users_router
+from fastapi import FastAPI
 from routers.auth import router as auth_router
+from routers.users import router as users_router
 
 
-app = FastAPI()
-app.include_router(users_router, prefix="/users", tags=["users"])
+app = FastAPI(
+    title="AuthService API",
+    version="1.0.0",
+    openapi_url="/openapi.json",
+    docs_url="/docs",
+    redoc_url="/redoc",
+    root_path="/auth",
+    root_path_in_servers=True
+)
+
+
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
+app.include_router(users_router, prefix="/users", tags=["users"])
 
 
 @app.get("/")
